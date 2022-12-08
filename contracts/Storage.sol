68dc4bf5e1c88b4bbff0bd53db12f2435a868177
MayankVats <vats.2610@gmail.com>
2022-12-08 19:02:37 +0530

    add storage

---
+++ contracts/Storage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ */
+contract Storage {
+    uint256 number;
+
+    /**
+     * @dev Store value in variable
+     * @param num value to store
+     */
+
+    function storePublic(uint256 num) public {
+        number = num;
+    }
+
+    function storeExternal(uint256 num) external {
+        number = num;
+    }
+
+    /**
+     * @dev Return value
+     * @return value of 'number'
+     */
+    function retrieve() public view returns (uint256) {
+        return number;
+    }
+}
